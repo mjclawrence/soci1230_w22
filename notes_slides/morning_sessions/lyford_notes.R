fae6d85018733be1010ca6a077b8b66e13c9b02b
mjclawrence <32300703+mjclawrence@users.noreply.github.com>
2022-01-11 11:49:46 -0500

    adds notebook_01_02

--- notes_slides/morning_sessions/lyford_notes.R
+++ notes_slides/morning_sessions/lyford_notes.R
@@ -57,8 +57,61 @@ ggplot(data = diamonds, aes(x = carat, y = price)) +
   geom_point()
 
 ## Use the scales package to change axis label formats
+library(scales)
 ggplot(data = diamonds, aes(x = carat, y = price)) +
   scale_y_continuous(labels=scales::dollar_format())
 
 ggplot(diamonds, aes(carat, price)) +
   scale_y_continuous(labels=dollar_format())
+
+## Tuesday, January 11
+
+# Let's make a graph visualizing the relationship between
+# the color of a diamond and its price
+
+ggplot(data = diamonds, aes(x = color, y = price)) + 
+  geom_boxplot()
+
+# Let's try faceting
+
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = carat, y = price, color = color)) +
+  facet_grid(~color)
+
+## Facet by two variables
+
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = carat, y = price, color = color)) +
+  facet_grid(clarity~color)
+
+## Let's focus on some of the cheaper diamonds
+
+ggplot(data = diamonds) +
+  geom_histogram(mapping = aes(x = price), bins = 100) +
+  xlim(0, 5000)
+
+ggplot(data = diamonds) +
+  geom_histogram(mapping = aes(x = price), bins = 100) +
+  xlim(0, 2000)
+
+ggplot(data = diamonds) +
+  geom_histogram(mapping = aes(x = price)) +
+  xlim(0, 2000)
+
+## Introducing Data Wrangling
+
+### The filter function keeps rows
+
+### Suppose I want to buy only diamonds that cost more than 10000
+
+filter(diamonds, price > 10000)
+
+### And only expensive diamonds of the best color (D)
+
+filter(diamonds, price>10000, color == "D")
+
+desirable.clarities <- c("I1", "SI1", "SI2")
+
+filter(diamonds, carat>1.5 & clarity %in% desirable.clarities)
+
+### %in% 
