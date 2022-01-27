5282b48e8abc95bd9acc16a834e77bb760a5f06e
mjclawrence <32300703+mjclawrence@users.noreply.github.com>
2022-01-27 12:37:39 -0500

    adds tidycensus example

---
+++ notebooks/tidycensus_example.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(tidycensus)
+library(leaflet)
+
+#acs_vars <- load_variables(2019, "acs5")
+
+state_population <- get_acs(
+  geography = "state",
+  variables = "B01001_001",
+  year = 2019,
+  geometry = TRUE
+)
+
+## For Leaflet
+
+
+color <- colorNumeric(palette = "viridis",
+                      domain = state_population$estimate)
+
+
+leaflet() |> 
+  addProviderTiles(providers$Stamen.TonerLite) |> 
+  addPolygons(data = state_population,
+              color = "black",
+              opacity = 1,
+              fillColor = ~color(estimate),
+              fillOpacity = 1,
+              stroke = 1,
+              weight = 1,
+              label = ~paste(NAME,": ", estimate)) |> 
+  setView(-96, 37.8, 3.5) |> 
+  addLegend(
+    position = "bottomright",
+    pal = color,
+    values = state_population$estimate,
+    title = "State Population"
+  )
