6c7caa2f249507cd97a98cf0e5a3facc51db4039
mjclawrence <32300703+mjclawrence@users.noreply.github.com>
2022-02-22 21:35:50 -0500

    fixes k_rank_cond_parq123

--- notebooks/scratchpad.R
+++ notebooks/scratchpad.R
@@ -59,3 +59,75 @@ tfs_means |>
                                                  w = n_responses),
             CHOOSE05.veryimportant = weighted.mean(CHOOSE12.veryimportant_mean + CHOOSE12.somewhatimportant_mean, 
                                                    w = n_responses))
+
+
+
+### Fixing k_rank_cond_parq123
+
+library(tidyverse)
+library(weights)
+
+mobility <- read_csv("data/css_means_mobility_withno2.csv")
+names <- read_csv("data/collnames.csv")
+chetty <- read_csv("data/chetty_fouryr.csv")
+
+wtd.quantile(chetty$mr_kq5_pq1, weights = chetty$count, prob = c(.9))
+
+chetty2 <- chetty |> 
+  mutate(k_rank_cond_parq123 = k_rank_cond_parq1*par_q1 + k_rank_cond_parq2*par_q1 + 
+           k_rank_cond_parq3*par_q3,
+         k_rank_cond_parq123_rev = k_rank_cond_parq123 / ((par_q1 + par_q2 + par_q3)),
+         k_rank_manual = ((k_rank_cond_parq123 + k_rank_cond_parq4*par_q4 + k_rank_cond_parq5*par_q5) / 
+                            (par_q1 + par_q2 + par_q3 + par_q4 + par_q5)),
+         kq345_cond_parq1 = kq3_cond_parq1 + kq4_cond_parq1 + kq5_cond_parq1)
+
+summary(chetty2$k_rank_manual)
+summary(chetty2$k_rank)
+
+`
+|> 
+  summarise(p75_mr_kq5_pq1 = wtd.quantile(mr_kq5_pq1, weights = count, prob = c(.75)),
+            p75_kq5_cond_parq1 = wtd.quantile(kq5_cond_parq1, weights = count, prob = c(.75)),
+            p75_k_rank_cond_parq123 = wtd.quantile(k_rank_cond_parq123, weights = count, prob = c(.75)),
+            p75_k_rank_cond_parq123_rev = wtd.quantile(k_rank_cond_parq123_rev, weights = count, prob = c(.75)),
+            p75_kq345_cond_parq1 = wtd.quantile(kq345_cond_parq1, weights = count, prob = c(.75)))
+
+
+summary(chetty$k_rank_cond_parq1)
+
+krank_vars <- c("k_rank_cond_parq1", "k_rank_cond_parq2", "k_rank_cond_parq3")
+
+chetty |> mean(krank_vars, na.rm = TRUE)
+
+
+
+
+middlebury <- chetty |> 
+  filter(name == "Middlebury College") |> 
+  select(name, starts_with("k_rank"), starts_with("par_q"))
+
+middlebury %>%
+  mutate(rank_1 = k_rank_cond_parq1 * par_q1,
+         rank_2 = k_rank_cond_parq2 * par_q2,
+         rank_3 = k_rank_cond_parq3 * par_q3,
+         rank_4 = k_rank_cond_parq4 * par_q4,
+         rank_5 = k_rank_cond_parq5 * par_q5,
+         par_q123 = par_q1 + par_q2 + par_q3) %>%
+  mutate(mymean = rowSums(select(., rank_1:rank_3)), na.rm = TRUE) %>% 
+  mutate(k_rank_cond_parq123_rev = mymean / par_q123) |> 
+  select(k_rank, mymean, par_q123, k_rank_cond_parq123_rev)
+
+middlebury |> 
+  mutate(newrank = 
+           (k_rank_cond_parq1*par_q1 +
+              k_rank_cond_parq2*par_q2 +
+              k_rank_cond_parq3*par_q3) /
+           (par_q1 + par_q2 + par_q3)
+  ) |> 
+  select(newrank)
+
+
+mutate(rowMeans(select(., starts_with("imp")), na.rm = TRUE))
+
+
+rowMeans(.[,-1:-2]))
